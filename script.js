daf7e6be3a69bc16dd1cda0eb71ef1916925ad14
Johannes Alves <johannes001002@gmail.com>
2022-03-04 16:04:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const keys = document.querySelectorAll('.numbers-operators')
+
+
+function calc(){
+    for(key of keys){
+        console.log(key)
+        
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
+
+calc()
